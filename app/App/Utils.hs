1200233bc0fcaa6567a59a920400a4bb2d8ca2ae
Filippo Sestini <sestini.filippo@gmail.com>
2023-04-27 11:15:08 +0100

    Use different name for Reader constructor

--- app/App/Utils.hs
+++ app/App/Utils.hs
@@ -5,11 +5,11 @@ module App.Utils where
 import Hs2Agda.Plugin
 
 {-# ANN type Reader HS2Agda #-}
-data Reader r a = Reader (r -> a)
+data Reader r a = MkReader (r -> a)
 
 {-# ANN bindReader HS2Agda #-}
 bindReader :: Reader r a -> (a -> Reader r b) -> Reader r b
-bindReader (Reader f) h =
-  Reader (\r ->
+bindReader (MkReader f) h =
+  MkReader (\r ->
     case h (f r) of
-      (Reader g) -> g r)
+      (MkReader g) -> g r)
